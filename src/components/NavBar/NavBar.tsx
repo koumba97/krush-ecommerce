6f20e0c0a0bceb930ee961a1ea708eac3967b70c
Koumba <koumba.keita@vadesecure.com>
2023-07-23 19:30:37 +0200

    Add Searchbar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,15 @@
 import "./NavBar.scss";
 import Logo from "../Logo/Logo";
+import SearchBar from "../SearchBar/SearchBar";
 
 const NavBar = () => {
   return (
     <nav>
       <Logo />
-      <div className="nav-content">
+
+      <SearchBar />
+
+      <div className="links-container">
         <a className="nav-link">
           <i className="lar la-user-circle"></i>
         </a>
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,12 @@
+import "./SearchBar.scss";
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Search for items and brands" />
+      <i className="las la-search"></i>
+    </div>
+  );
+};
+
+export default SearchBar;
