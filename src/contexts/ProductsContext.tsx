fc91e4076e04714052448bbedb4a3c76d8c523c2
Koumba <koumba.keita@vadesecure.com>
2023-10-20 21:36:28 +0200

    Add addCollectionAndDocuments & getCategoriesAndDocuments firebase util functions

--- src/contexts/ProductsContext.tsx
+++ src/contexts/ProductsContext.tsx
@@ -1,6 +1,8 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { ProductsData } from '../data/ProductsData';
 import { Product } from '../types/Product';
+import { addCollectionAndDocuments, getCategoriesAndDocuments } from '../utils/firebase/firebase';
+import { ShopData } from '../data/ShopData';
 
 export const ProductsContext = createContext({
     products: [] as Product[],
@@ -12,6 +14,19 @@ interface IProp {
 
 export const ProductsProvider = ({ children }: IProp) => {
     const [products, _setProducts] = useState(ProductsData);
+
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', ShopData);
+    // }, []);
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+        };
+        getCategoriesMap();
+    }, []);
+
     const value = { products };
     return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>;
 };
--- src/utils/firebase/firebase.ts
+++ src/utils/firebase/firebase.ts
@@ -10,7 +10,17 @@ import {
     signOut,
     onAuthStateChanged,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs,
+    QuerySnapshot,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -40,6 +50,32 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 export const signInWithFacebookPopup = () => signInWithPopup(auth, facebookProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey: string, objectsToAdd: any) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object: any) => {
+        const docRef = doc(collectionRef, object.title.toLowerCase());
+        batch.set(docRef, object);
+    });
+
+    await batch.commit();
+};
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc: any, docSnapshot: any) => {
+        const { title, items } = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {});
+
+    return categoryMap;
+};
 
 export const createUserDocumentFromAuth = async (userAuth: any, additionalInformation?: any) => {
     if (!userAuth) return;
